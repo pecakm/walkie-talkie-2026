266ef5051d3a885ef823a22ad69e568fa28229b6
Mikołaj Pęcak <mikolajpecak@gmail.com>
2026-05-27 11:20:42 +0200

    Add next-intl and Livekit libraries

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
+import { cookies } from 'next/headers';
 import { Geist } from 'next/font/google';
+import { NextIntlClientProvider } from 'next-intl';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v16-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 
@@ -18,21 +20,26 @@ export const metadata: Metadata = {
   description: 'A simple walkie talkie app created by Mikolaj Pecak',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = await cookies();
+  const locale = cookieStore.get('locale')?.value || 'en';
+
   return (
-    <html lang="en" className={geist.className}>
+    <html lang={locale} className={geist.className}>
       <body>
-        <AppRouterCacheProvider>
-          <ThemeProvider theme={theme}>
-            <StyledComponentsRegistry>
-              {children}
-            </StyledComponentsRegistry>
-          </ThemeProvider>
-        </AppRouterCacheProvider>
+        <NextIntlClientProvider>
+          <AppRouterCacheProvider>
+            <ThemeProvider theme={theme}>
+              <StyledComponentsRegistry>
+                {children}
+              </StyledComponentsRegistry>
+            </ThemeProvider>
+          </AppRouterCacheProvider>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 'use client';
 
+import { useTranslations } from 'next-intl';
+
 import { Button } from '@/components';
 
 import { Container } from './page.styled';
 
-export default function Home() {
+export default function HomePage() {
+  const t = useTranslations('homePage');
+
   return (
     <Container>
-      <Button onClick={() => {}}>Join</Button>
+      <Button onClick={() => {}}>{t('join')}</Button>
     </Container>
   );
 }
